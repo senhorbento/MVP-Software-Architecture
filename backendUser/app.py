1414696bc5206f6f8fe240f3084012d317515441
senhorbento <sbento.ti@gmail.com>
2024-07-05 18:32:47 -0300

    Feat: CRUD user

--- backendUser/app.py
+++ backendUser/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
-import requests
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
+import requests
 
 app = Flask(__name__)
 
@@ -24,8 +25,201 @@ app.config['SWAGGER'] = {
     ]
 }
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 swagger = Swagger(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    access_level = db.Column(db.Integer, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+def get_access_level_by_id(user_id):
+    user = User.query.get_or_404(user_id)
+    return user.access_level
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    """
+    Criar um novo usuário
+    ---
+    parameters:
+        - name: body
+          in: body
+          required: True
+          schema:
+            type: object
+            required:
+                - login
+                - password
+                - access_level
+            properties:
+                login:
+                    type: string
+                    example: "user1"
+                password:
+                    type: string
+                    example: "pass123"
+                access_level:
+                    type: integer
+                    example: 1
+    responses:
+        201:
+            description: Usuário criado com sucesso
+        400:
+            description: Erro na requisição
+    """
+    data = request.get_json()
+    new_user = User(
+        login=data['login'], password=data['password'], access_level=data['access_level'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Usuário criado com sucesso'}), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    """
+    Obter todos os usuários
+    ---
+    responses:
+        200:
+            description: Lista de usuários
+            schema:
+                type: array
+                items:
+                    type: object
+                    properties:
+                        id:
+                            type: integer
+                            example: 1
+                        login:
+                            type: string
+                            example: "user1"
+                        password:
+                            type: string
+                            example: "pass123"
+                        access_level:
+                            type: integer
+                            example: 1
+    """
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {'id': user.id, 'login': user.login,
+                     'password': user.password, 'access_level': user.access_level}
+        output.append(user_data)
+    return jsonify(output), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    """
+    Obter um usuário específico por ID
+    ---
+    parameters:
+        - name: user_id
+          in: path
+          type: integer
+          required: True
+          description: ID do usuário
+    responses:
+        200:
+            description: Detalhes do usuário
+            schema:
+                type: object
+                properties:
+                    id:
+                        type: integer
+                        example: 1
+                    login:
+                        type: string
+                        example: "user1"
+                    password:
+                        type: string
+                        example: "pass123"
+                    access_level:
+                        type: integer
+                        example: 1
+        404:
+            description: Usuário não encontrado
+    """
+    user = User.query.get_or_404(user_id)
+    user_data = {'id': user.id, 'login': user.login,
+                 'password': user.password, 'access_level': user.access_level}
+    return jsonify(user_data), 200
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    """
+    Atualizar um usuário existente
+    ---
+    parameters:
+        - name: user_id
+          in: path
+          type: integer
+          required: True
+          description: ID do usuário
+        - name: body
+          in: body
+          required: True
+          schema:
+            type: object
+            required:
+                - login
+                - password
+                - access_level
+            properties:
+                login:
+                    type: string
+                    example: "user1"
+                password:
+                    type: string
+                    example: "pass123"
+                access_level:
+                    type: integer
+                    example: 1
+    responses:
+        200:
+            description: Usuário atualizado com sucesso
+        400:
+            description: Erro na requisição
+        404:
+            description: Usuário não encontrado
+    """
+    data = request.get_json()
+    user = User.query.get_or_404(user_id)
+    user.login = data['login']
+    user.password = data['password']
+    user.access_level = data['access_level']
+    db.session.commit()
+    return jsonify({'message': 'Usuário atualizado com sucesso'}), 200
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """
+    Excluir um usuário existente
+    ---
+    parameters:
+        - name: user_id
+          in: path
+          type: integer
+          required: True
+          description: ID do usuário
+    responses:
+        200:
+            description: Usuário excluído com sucesso
+        404:
+            description: Usuário não encontrado
+    """
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'Usuário excluído com sucesso'}), 200
 
 @app.route('/external/posts/<int:user_id>', methods=['GET'])
 def get_posts(user_id):
@@ -61,10 +255,12 @@ def get_posts(user_id):
         401:
             description: Não Autorizado
     """
+    acess_level = get_access_level_by_id(user_id)
+    if acess_level < 1:
+        return jsonify({'error': 'Não Autorizado'}), 401
     response = requests.get('http://127.0.0.1:5001/posts')
     posts = response.json()
     return jsonify(posts), 200
-
 
 @app.route('/external/posts/<int:post_id>/<int:user_id>', methods=['GET'])
 def get_post(post_id, user_id):
@@ -103,14 +299,16 @@ def get_post(post_id, user_id):
         401:
             description: Não Autorizado
     """
+    acess_level = get_access_level_by_id(user_id)
+    if acess_level < 1:
+        return jsonify({'error': 'Não Autorizado'}), 401
     response = requests.get(
         f'http://127.0.0.1:5001/posts/{post_id}')
     post = response.json()
     return jsonify(post), 200
 
-
 @app.route('/external/cep/<string:cep>/<int:user_id>', methods=['GET'])
-def get_cep(cep):
+def get_cep(cep, user_id):
     """
     Obter informações sobre um CEP, desde que o user tenha acesso.
     ---
@@ -168,6 +366,9 @@ def get_cep(cep):
         404:
             description: CEP não encontrado
     """
+    acess_level = get_access_level_by_id(user_id)
+    if acess_level < 2:
+        return jsonify({'error': 'Não Autorizado'}), 401
     response = requests.get(f'http://127.0.0.1:5001/ws/{cep}/json/')
     if response.status_code == 200:
         data = response.json()
@@ -175,7 +376,6 @@ def get_cep(cep):
             return jsonify({'error': 'CEP não encontrado'}), 404
         return jsonify(data), 200
     return jsonify({'error': 'CEP inválido'}), 400
-
 
 if __name__ == '__main__':
     port = 5002
